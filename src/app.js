7b97aa75c8acd2e160d1c25c1475106cd259f5b0
Asaph Fernandes <asaphfernandes@hotmail.com>
2020-04-16 00:07:51 -0300

    challenge concluded

--- src/app.js
+++ src/app.js
@@ -1,33 +1,75 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use((request, _response, next) => {
+  let message = `[${request.method}]: ${request.url}`;
+  console.time(message);
+  next();
+  console.timeEnd(message);
+});
+
+const validateUuid = (request, response, next) => {
+
+  if (request.params.id && !isUuid(request.params.id)) {
+    return response.status(400).json({ error: 'Invalid uuid' });
+  }
+
+  return next();
+};
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+
+  response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateUuid, (request, response) => {
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex(item => item.id === request.params.id);
+  
+  if (index < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  let repository = repositories[index];
+  repository = { ...repository, title, url, techs };
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateUuid, (request, response) => {
+  const index = repositories.findIndex(item => item.id === request.params.id);
+  if (index < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+  repostoirs = repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateUuid, (request, response) => {
+  const index = repositories.findIndex(item => item.id === request.params.id);
+  
+  if (index < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  let repository = repositories[index];
+  ++repository.likes;
+
+  return response.status(201).json(repository);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Desafio 02: Conceitos do Node.js - %cAsaph Fernandes', 'color: blue');
+});
